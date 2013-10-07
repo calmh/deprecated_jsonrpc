b5394f3edc1ec3729f5f9b97b79b1b9c1f64792f
Jakob Borg <jakob@nym.se>
2013-10-07 14:58:30 +0200

    doc.go

---
+++ doc.go
@@ -0,0 +1,6 @@
+/*
+Package jsonrpc implements a standards compliant, asynchronous JSON-RPC client.
+The JSON-RPC 2.0 standard as specified in http://www.jsonrpc.org/specification
+is supported, while it is also possible to implement vendor specific dialects.
+*/
+package jsonrpc
